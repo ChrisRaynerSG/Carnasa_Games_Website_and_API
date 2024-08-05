0f9b3f58c1e20731dfa075549b186ed989ca2bb6
Chris Rayner <crayner@spartaglobal.com>
2024-08-05 11:52:45 +0100

    init setup of project

---
+++ CarnasaGamesWebsiteAndApi/src/main/java/com/sparta/cr/carnasagameswebsiteandapi/CarnasaGamesWebsiteAndApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cr.carnasagameswebsiteandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnasaGamesWebsiteAndApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarnasaGamesWebsiteAndApiApplication.class, args);
+    }
+
+}
---
+++ CarnasaGamesWebsiteAndApi/src/test/java/com/sparta/cr/carnasagameswebsiteandapi/CarnasaGamesWebsiteAndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cr.carnasagameswebsiteandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnasaGamesWebsiteAndApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
