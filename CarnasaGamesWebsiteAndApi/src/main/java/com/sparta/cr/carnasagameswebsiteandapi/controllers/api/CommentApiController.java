13e92c8b844cd3382c1db9886d74a5cb9981c508
Chris Rayner <crayner@spartaglobal.com>
2024-08-13 10:36:53 +0100

    updated CommentsApiController to hide private Users

--- CarnasaGamesWebsiteAndApi/src/main/java/com/sparta/cr/carnasagameswebsiteandapi/controllers/api/CommentApiController.java
+++ CarnasaGamesWebsiteAndApi/src/main/java/com/sparta/cr/carnasagameswebsiteandapi/controllers/api/CommentApiController.java
@@ -1,5 +1,6 @@
 package com.sparta.cr.carnasagameswebsiteandapi.controllers.api;
 
+import com.sparta.cr.carnasagameswebsiteandapi.annotations.CurrentOwner;
 import com.sparta.cr.carnasagameswebsiteandapi.exceptions.globalexceptions.ModelNotFoundException;
 import com.sparta.cr.carnasagameswebsiteandapi.exceptions.userexceptions.UserNotFoundException;
 import com.sparta.cr.carnasagameswebsiteandapi.models.CommentModel;
@@ -13,6 +14,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -35,55 +37,72 @@ public class CommentApiController {
     }
 
     @GetMapping("/search/all")
-    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllComments() {
+    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllComments(@CurrentOwner String username,
+                                                                                     Authentication authentication) {
         List<EntityModel<CommentModel>> comments = commentService.getAllComments()
-                .stream().map(this::getCommentEntityModel).toList();
+                .stream().map(comment -> getCommentEntityModel(comment, username, authentication)).toList();
         return new ResponseEntity<>(CollectionModel.of(comments).add(
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getAllComments()).withSelfRel())
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getAllComments(username,authentication)).withSelfRel())
                 , HttpStatus.OK);
     }
     @GetMapping("/search/{commentId}")
-    public ResponseEntity<EntityModel<CommentModel>> getCommentById(@PathVariable Long commentId) {
+    public ResponseEntity<EntityModel<CommentModel>> getCommentById(@PathVariable Long commentId,
+                                                                    @CurrentOwner String username,
+                                                                    Authentication authentication) {
         if(commentService.getComment(commentId).isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         CommentModel commentModel = commentService.getComment(commentId).get();
-        return ResponseEntity.ok(getCommentEntityModel(commentModel));
+        return ResponseEntity.ok(getCommentEntityModel(commentModel,username,authentication));
     }
     @GetMapping("/search/game/{gameId}")
-    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByGame(@PathVariable Long gameId) {
+    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByGame(@PathVariable Long gameId,
+                                                                                           @CurrentOwner String username,
+                                                                                           Authentication authentication) {
         if(gameService.getGame(gameId).isEmpty()) {
             throw new ModelNotFoundException("Game with ID: " + gameId + " not found");
         }
-       List<EntityModel<CommentModel>> comments = commentService.getCommentsByGame(gameId).stream().map(this::getCommentEntityModel).toList();
+       List<EntityModel<CommentModel>> comments = commentService.getCommentsByGame(gameId).stream().map(
+               comment -> getCommentEntityModel(comment, username, authentication)
+       ).toList();
        if(comments.isEmpty()) {
            return ResponseEntity.notFound().build();
        }
        return new ResponseEntity<>(CollectionModel.of(comments), HttpStatus.OK);
     }
     @GetMapping("/search/user/{userId}")
-    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByUser(@PathVariable Long userId) {
+    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByUser(@PathVariable Long userId,
+                                                                                           @CurrentOwner String username,
+                                                                                           Authentication authentication) {
         if(userService.getUser(userId).isEmpty()) {
             throw new UserNotFoundException(userId.toString());
         }
-        List<EntityModel<CommentModel>> comments = commentService.getCommentsByUser(userId).stream().map(this::getCommentEntityModel).toList();
+        List<EntityModel<CommentModel>> comments = commentService.getCommentsByUser(userId).stream().map(
+                comment -> getCommentEntityModel(comment, username, authentication)).toList();
         if(comments.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return new ResponseEntity<>(CollectionModel.of(comments), HttpStatus.OK);
     }
     @GetMapping("/search/date/today")
-    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByDateToday() {
+    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByDateToday(@CurrentOwner String username,
+                                                                                                Authentication authentication) {
         LocalDate today = LocalDate.now();
-        List<EntityModel<CommentModel>> comments = commentService.getCommentsFromToday(today).stream().map(this::getCommentEntityModel).toList();
+        List<EntityModel<CommentModel>> comments = commentService.getCommentsFromToday(today).stream().map(
+                comment -> getCommentEntityModel(comment,username,authentication)).toList();
         if(comments.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return new ResponseEntity<>(CollectionModel.of(comments), HttpStatus.OK);
     }
     @GetMapping("/search/date/{date1}/{date2}")
-    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByDate(@PathVariable String date1, @PathVariable String date2) {
-        List<EntityModel<CommentModel>> comments = commentService.getCommentsByDate(date1, date2).stream().map(this::getCommentEntityModel).toList();
+    public ResponseEntity<CollectionModel<EntityModel<CommentModel>>> getAllCommentsByDate(@PathVariable String date1,
+                                                                                           @PathVariable String date2,
+                                                                                           @CurrentOwner String username,
+                                                                                           Authentication authentication) {
+
+        List<EntityModel<CommentModel>> comments = commentService.getCommentsByDate(date1, date2).stream().map(
+                comment -> getCommentEntityModel(comment, username, authentication)).toList();
         if(comments.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
@@ -91,17 +110,20 @@ public class CommentApiController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<EntityModel<CommentModel>> createComment(@RequestBody CommentModel commentModel) {
+    public ResponseEntity<EntityModel<CommentModel>> createComment(@RequestBody CommentModel commentModel,
+                                                                   @CurrentOwner String username,
+                                                                   Authentication authentication) {
         if(!commentService.validateNewComment(commentModel)){
             return ResponseEntity.badRequest().build();
         }
         CommentModel newComment = commentService.createComment(commentModel);
         URI location = URI.create("/api/comments/search/"+newComment.getId());
-        Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getCommentById(newComment.getId())).withSelfRel();
+        Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getCommentById(newComment.getId(), username, authentication)).withSelfRel();
         return ResponseEntity.created(location).body(EntityModel.of(newComment).add(selfLink));
     }
     @PutMapping("/update/{commentId}")
-    public ResponseEntity updateComment(@PathVariable Long commentId, @RequestBody CommentModel commentModel) {
+    public ResponseEntity updateComment(@PathVariable Long commentId, @RequestBody CommentModel commentModel,
+                                        @CurrentOwner String username, Authentication authentication) {
         if(commentService.getComment(commentId).isEmpty()) {
             return ResponseEntity.notFound().build();
         }
@@ -116,7 +138,8 @@ public class CommentApiController {
     }
 
     @DeleteMapping("/delete/{commentId}")
-    public ResponseEntity deleteComment(@PathVariable Long commentId) {
+    public ResponseEntity deleteComment(@PathVariable Long commentId, @CurrentOwner String username,
+                                        Authentication authentication) {
         if(commentService.getComment(commentId).isEmpty()) {
             return ResponseEntity.notFound().build();
         }
@@ -124,25 +147,35 @@ public class CommentApiController {
         return ResponseEntity.noContent().build();
     }
 
-    private Link getUserLink(CommentModel commentModel){
-        return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                .methodOn(UserApiController.class)
-                .getUserById(commentModel.getUserModel().getId()))
-                .withRel("User: " + commentModel.getUserModel().getUsername());
+    private Link getUserLink(CommentModel commentModel, String currentUser, Authentication authentication){
+        if(isUserAccountPrivateAndHidden(commentModel,currentUser,authentication)){
+            return Link.of("").withRel("Private user");
+        }
+        else {
+            return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                            .methodOn(UserApiController.class)
+                            .getUserById(commentModel.getUserModel().getId(),currentUser,authentication))
+                    .withRel("User: " + commentModel.getUserModel().getUsername());
+        }
     }
 
-    private Link getGameLink(CommentModel commentModel){
+    private Link getGameLink(CommentModel commentModel, String currentUser, Authentication authentication){
         return WebMvcLinkBuilder
                 .linkTo(WebMvcLinkBuilder
-                .methodOn(GameApiController.class).getGameById(commentModel.getGamesModel().getId()))
+                .methodOn(GameApiController.class).getGameById(commentModel.getGamesModel().getId(), currentUser, authentication))
                 .withRel("Game: " + commentModel.getGamesModel().getTitle());
     }
 
-    private EntityModel<CommentModel> getCommentEntityModel(CommentModel commentModel) {
-        Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getCommentById(commentModel.getId())).withSelfRel();
-        Link relink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getAllComments()).withRel("All Comments");
-        return EntityModel.of(commentModel, selfLink, relink, getUserLink(commentModel), getGameLink(commentModel));
+    private EntityModel<CommentModel> getCommentEntityModel(CommentModel commentModel, String currentUser, Authentication authentication) {
+        Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getCommentById(commentModel.getId(), currentUser, authentication)).withSelfRel();
+        Link relink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CommentApiController.class).getAllComments(currentUser,authentication)).withRel("All Comments");
+        return EntityModel.of(commentModel, selfLink, relink, getUserLink(commentModel, currentUser, authentication), getGameLink(commentModel, currentUser, authentication));
     }
 
+    private boolean isUserAccountPrivateAndHidden(CommentModel commentModel, String currentUser, Authentication authentication){
+        return commentModel.getUserModel().isPrivate()
+                && authentication.getAuthorities().stream().noneMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"))
+                && !commentModel.getUserModel().getUsername().equals(currentUser);
 
+    }
 }
